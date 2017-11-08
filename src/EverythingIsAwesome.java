180da7b2868e6044a63831a10378b8b97c6c57f4
CatcusCat <wjmorris2004@gmail.com>
2017-11-07 19:23:40 -0800

    Greeter
    EverythingIsAeseome

---
+++ src/EverythingIsAwesome.java
@@ -0,0 +1,12 @@
+import javax.swing.JOptionPane;
+
+public class EverythingIsAwesome {
+public static void main(String[] args) {
+	
+	String cool=JOptionPane.showInputDialog(" what is awesome?");
+	JOptionPane.showMessageDialog(null,cool+" is awesome");
+	
+}
+	
+	
+}
---
+++ src/Greeter.java
@@ -0,0 +1,21 @@
+import javax.swing.JOptionPane;
+
+public class Greeter {
+public static void main(String[] args) {
+	String name=JOptionPane.showInputDialog("Enter your name");
+JOptionPane.showMessageDialog(null, "Hi "+name);
+}
+
+	
+	
+	
+	
+	
+	
+	
+	
+
+}
+
+
+
